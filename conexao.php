fd3e2c78c9d641e1fb706ba42628a0760884394f
bijunkes <bianca.jr2007@gmail.com>
2025-11-25 09:05:43 -0300

    get_usuario(s)

--- conexao.php
+++ conexao.php
@@ -17,7 +17,7 @@
 
         return $stmt->execute();
     }
-    //cadastra_usuario("b", "b", "123");
+   //cadastra_usuario("C", "c@", "123");
 
     function delete_usuario($id) {
         $con = conecta_db();
@@ -34,12 +34,34 @@
 
         $stmt = $con->prepare("UPDATE usuarios SET nome = :nome, email = :email, senha = :senha WHERE id = :id");
 
+        $stmt->bindParam(':id', $id);
         $stmt->bindParam(':nome', $nome);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':senha', $senha);
-        $stmt->bindParam(':id', $id);
 
         return $stmt->execute();
     }
     //update_usuario(2, "B", "b@", "123");
+
+    function get_usuario($id) {
+        $con = conecta_db();
+
+        $stmt = $con->prepare("SELECT * FROM usuarios WHERE id = :id");
+
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    var_dump(get_usuario(2));
+
+    function get_usuarios() {
+        $con = conecta_db();
+
+        $stmt = $con->prepare("SELECT * FROM usuarios");
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    var_dump(get_usuarios());
 ?>
